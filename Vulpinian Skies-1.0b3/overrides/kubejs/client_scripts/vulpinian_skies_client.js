e75e0b7c38e7b021dd489269dfaf90bc29cfb70b
unknown <aiastesla@gmail.com>
2023-03-18 21:32:37 -0400

    new file:   Vulpinian Skies-1.0b3/

---
+++ Vulpinian Skies-1.0b3/overrides/kubejs/client_scripts/vulpinian_skies_client.js
@@ -0,0 +1,9 @@
+// priority: 0
+
+console.info('Hello, World! (You will see this line every time client resources reload)')
+
+onEvent('jei.hide.items', event => {
+	// Hide items in JEI here
+	// event.hide('minecraft:cobblestone')
+	event.hide('tconstruct:gadgets/throwball/efln_ball')
+})
