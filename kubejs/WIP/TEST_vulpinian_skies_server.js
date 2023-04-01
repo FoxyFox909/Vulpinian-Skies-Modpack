ac73cc3c7fa783bbffa5399ea2e3d43c299c1dff
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-04-01 03:14:28 -0400

    Add files via upload

--- kubejs/WIP/TEST_vulpinian_skies_server.js
+++ kubejs/WIP/TEST_vulpinian_skies_server.js
@@ -153,8 +153,10 @@ onEvent("command.registry", event => {
 						let cardTag = entityData.getInventory().getItem(slot).getTag().Inscribed //get the NBT dat adown to the Inscribed key for validation
 						
 						if (cardTag == "Vulpine Co. [stage1-valid]") {
+							if (entityData.stages.has('ga_registered_player')) {event.server.tell("Found a valid Card, but you are already registered!"); return;}
 							let currentPlayersCount = event.server.persistentData.currentPlayersCount.length
 							if (currentPlayersCount >= maxPlayers) {event.server.tell("Found a valid Card, but player slots are full! Please wait until the next match."); return;}
+							//if (matchOngoing) {event.server.tell("Found a valid Card, but a match is currently underway! Please wait until the next match. Go watch some pew pews in the meantime :)"); return;} future-proofing
 							let nameString = entityData.getInventory().getItem(slot).getTag().display.Name // get Item display name
 							let nameRegex = /(.")(,)/
 							let nameIndex = nameString.search(nameRegex)
@@ -167,14 +169,14 @@ onEvent("command.registry", event => {
 							stringifiedLore = stringifiedLore + loreString;
 							let loreRegex = /STATUS/
 							let loreIndex = stringifiedLore.search(loreRegex)
-							let newStatus = `Prematch - Registered to ${stringifiedPlayerName}`
+							let newStatus = `Prematch - Registered to ${stringifiedPlayerName} as Player ${currentPlayersCount + 1}`
 							let modifiedLore = stringifiedLore.substring(0, (loreIndex + 8)) + newStatus + stringifiedLore.substring((loreIndex + 26))
-							event.server.tell("Found a valid Card!"); //not sure if this will whisper or tell the whole server
+							event.server.tell(`Found a valid Card! You have been registered as Player ${currentPlayersCount + 1}.`); //not sure if this will whisper or tell the whole server
 							entityData.getInventory().getItem(slot).getTag().display.Name = modifiedName;
 							entityData.getInventory().getItem(slot).getTag().Inscribed = "Vulpine Co. [stage2-prematch]"; //Set the NBT tag to stage2
 							entityData.getInventory().getItem(slot).getTag().display.Lore = eval(modifiedLore);
 							pushCurrentPlayers(stringifiedPlayerName, (currentPlayersCount + 1));
-							
+							entityData.addTag('ga_registered_player')
 							
 							break;
 						} else {continue};
@@ -210,6 +212,18 @@ onEvent("command.registry", event => {
 					return 1
 					})
 				)
+			)
+			.then(Commands.literal('test').executes(ctx => {
+				let tags = ctx.source.entity.stages.has('test_tag')
+				ctx.source.entity.addTag('test_tag')
+				Utils.server.tell("My tags are: " + tags)
+				Utils.server.tell("Type is: " + typeof(tags))
+				//Utils.server.tell("Contains test_tag: " + tags.includes("test_tag"))
+				//Add code here
+				//For copy-pasting purposes
+				//KEEP RETURN
+				return 1
+				})
 			)
 			.then(Commands.literal('BLANK').executes(ctx => {
 
