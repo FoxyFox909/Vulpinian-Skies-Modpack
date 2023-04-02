b969b6374f435a60b1a2c1b46af92ca6c0034962
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-04-01 20:10:43 -0400

    Add files via upload

--- kubejs/WIP/TEST_vulpinian_skies_server.js
+++ kubejs/WIP/TEST_vulpinian_skies_server.js
@@ -1,5 +1,11 @@
 
-const maxPlayers =  4//Max amount of players allowed to be registered to a game
+const maxPlayers =  4  //Max amount of players allowed to be registered to a match
+const minPlayers =  2  //Min amount of players needed to trigger a match
+const prematchTimerDefault = 30 //Default time (in 20-ticks intervals, meaning seconds) before a match starts once minPlayers has been satisfied
+
+//Variables for code operations, do not touch
+var prematchTimer = 30
+var isPrematchTimerTicking = false
 
 onEvent('item.entity_interact', event => {
 	
@@ -64,13 +70,13 @@ onEvent("command.registry", event => {
 	function addToLifetimePlayers(number){
 		let data = event.server.persistentData.gunArenaLifetimePlayersCount
 		event.server.persistentData.gunArenaLifetimePlayersCount = event.server.persistentData.gunArenaLifetimePlayersCount + number
-		return data
+		return data;
 	}
 	
 	function resetLifetimePlayers(){
 		let data = event.server.persistentData.gunArenaLifetimePlayersCount
 		event.server.persistentData.gunArenaLifetimePlayersCount = 0//{"count": 1}
-		return data
+		return data;
 	}
 	
 	function initializeCurrentPlayers(pvalue, resetBool){
@@ -84,11 +90,35 @@ onEvent("command.registry", event => {
 	}
 	
 	function pushCurrentPlayers(playerName, playerNumber){
-		const currentPlayers = event.server.persistentData.gunArenaCurrentPlayersCount
-		const player = `{"PlayerNumber":"${playerNumber}","Name":"${playerName}","Team":"None"}`
-		currentPlayers.push(player)
-		return
+		//const currentPlayers = event.server.persistentData.gunArenaCurrentPlayersCount
+		let player = `{"PlayerNumber":"${playerNumber}","Name":"${playerName}","Team":"None"}`
+		event.server.persistentData.gunArenaCurrentPlayersCount.push(JSON.parse(player))
+		return;
 	}
+	
+	function assignToTeam (playerCardInscribed, team) {
+		let i = 0;
+		
+		
+		
+		currentPlayersCount()[i].Team = (team)
+		return;
+		
+	}
+	
+	function changeLore() {
+		let i = 0;
+		let loreString = entityData.getInventory().getItem(slot).getTag().display.Lore // get Item lore
+		let stringifiedLore = ""
+		stringifiedLore = stringifiedLore + loreString;
+		let loreRegex = /STATUS/
+		let loreIndex = stringifiedLore.search(loreRegex)
+		entityData.getInventory().getItem(slot).getTag().display.Lore = eval(modifiedLore);
+		return;
+	}
+	
+	const lifetimePlayersCount = () => {return event.server.persistentData.gunArenaLifetimePlayersCount}
+	const currentPlayersCount = () => {return event.server.persistentData.gunArenaCurrentPlayersCount}
 	
 	event.register(
 		Commands.literal('vps-gunarena')
@@ -98,8 +128,6 @@ onEvent("command.registry", event => {
 					.then(Commands.argument('rel-pos-y', Arguments.INTEGER.create(event))
 						.then(Commands.argument('rel-pos-z', Arguments.INTEGER.create(event))
 							.executes(ctx => {
-								
-								var lifetimePlayers = event.server.persistentData.gunArenaLifetimePlayersCount
 								const relPosX = Arguments.INTEGER.getResult(ctx, "rel-pos-x");
 								const relPosY = Arguments.INTEGER.getResult(ctx, "rel-pos-y");
 								const relPosZ = Arguments.INTEGER.getResult(ctx, "rel-pos-z");
@@ -107,7 +135,7 @@ onEvent("command.registry", event => {
 								let posX = Math.floor(pos.x())
 								let posY = Math.floor(pos.y())
 								let posZ = Math.floor(pos.z())
-								let paddedId = lifetimePlayers.toString().padStart(5,0)
+								let paddedId = lifetimePlayersCount().toString().padStart(5,0)
 								
 								addToLifetimePlayers(1)
 								//Utils.server.runCommand('data merge block ' + (posX + relPosX)+ ' ' + (posY + relPosY) + ' ' + (posZ + relPosZ) + ` {Items:[{Slot:0b,id:"create_things_and_misc:card", Count: 1b, tag:{Inscribed:"Vulpine Co. [stage1-valid]",display:{Name:'[{"text":"Gun Arena ID-${paddedId}","italic":false}]'}}}]}`)
@@ -129,21 +157,26 @@ onEvent("command.registry", event => {
 			)
 			.then(Commands.literal('lifetime-players')
 				.then(Commands.literal('query').executes(ctx => {
-					let lifetimePlayers = event.server.persistentData.gunArenaLifetimePlayersCount
-					Utils.server.tell("Total Player Count is " + lifetimePlayers)
+					Utils.server.tell("Total Player Count is " + lifetimePlayersCount())
 					//Utils.server.tell("Type of Player Count is " + typeof(lifetimePlayers))
 					return 1
 					})
 				)
 				.then(Commands.literal('reset').executes(ctx => {
-					let lifetimePlayers = event.server.persistentData.gunArenaLifetimePlayersCount
-					Utils.server.tell("Total Player Count was " + lifetimePlayers)
+					Utils.server.tell("Total Player Count was " + lifetimePlayersCount())
 					resetLifetimePlayers()
-					Utils.server.tell("Total Player Count is now " + event.server.persistentData.gunArenaLifetimePlayersCount)
+					Utils.server.tell("Total Player Count is now " + lifetimePlayersCount())
 					return 1
 					})
 				)
-			)
+				.then(Commands.literal('test').executes(ctx => {
+					Utils.server.tell("Total Player Count was " + lifetimePlayersCount())
+					addToLifetimePlayers(1)
+					Utils.server.tell("Total Player Count is now " + lifetimePlayersCount())
+					return 1
+					})
+				)
+			) 
 			.then(Commands.literal('stage-two').executes(ctx => {
 				//Commences stage 2 of Card, setting Card Inscribed NBT to Stage 2 and binding card to player				
 					
@@ -166,8 +199,7 @@ onEvent("command.registry", event => {
 						
 						if (cardTag == "Vulpine Co. [stage1-valid]") {
 							if (entityData.stages.has('ga_registered_player')) {event.server.tell("Found a valid Card, but you are already registered!"); return;}
-							let currentPlayersCount = event.server.persistentData.gunArenaCurrentPlayersCount.length
-							if (currentPlayersCount >= maxPlayers) {event.server.tell("Found a valid Card, but player slots are full! Please wait until the next match."); return;}
+							if (currentPlayersCount().length >= maxPlayers) {event.server.tell("Found a valid Card, but player slots are full! Please wait until the next match."); return;}
 							//if (matchOngoing) {event.server.tell("Found a valid Card, but a match is currently underway! Please wait until the next match. Go watch some pew pews in the meantime :)"); return;} future-proofing
 							let nameString = entityData.getInventory().getItem(slot).getTag().display.Name // get Item display name
 							let nameRegex = /(.")(,)/
@@ -181,13 +213,13 @@ onEvent("command.registry", event => {
 							stringifiedLore = stringifiedLore + loreString;
 							let loreRegex = /STATUS/
 							let loreIndex = stringifiedLore.search(loreRegex)
-							let newStatus = `Prematch - Registered to ${stringifiedPlayerName} as Player ${currentPlayersCount + 1}`
+							let newStatus = `Prematch - Registered to ${stringifiedPlayerName} as Player ${currentPlayersCount().length + 1}`
 							let modifiedLore = stringifiedLore.substring(0, (loreIndex + 8)) + newStatus + stringifiedLore.substring((loreIndex + 26))
-							event.server.tell(`Found a valid Card! You have been registered as Player ${currentPlayersCount + 1}.`); //not sure if this will whisper or tell the whole server
+							event.server.tell(`Found a valid Card! You have been registered as Player ${currentPlayersCount().length + 1}.`); //not sure if this will whisper or tell the whole server
 							entityData.getInventory().getItem(slot).getTag().display.Name = modifiedName;
 							entityData.getInventory().getItem(slot).getTag().Inscribed = "Vulpine Co. [stage2-prematch]"; //Set the NBT tag to stage2
 							entityData.getInventory().getItem(slot).getTag().display.Lore = eval(modifiedLore);
-							pushCurrentPlayers(stringifiedPlayerName, (currentPlayersCount + 1));
+							pushCurrentPlayers(stringifiedPlayerName, (currentPlayersCount().length + 1));
 							entityData.addTag('ga_registered_player')
 							
 							break;
@@ -198,56 +230,57 @@ onEvent("command.registry", event => {
 				})
 			).then(Commands.literal('currentplayers')
 				.then(Commands.literal('query').executes(ctx => {
-					var currentPlayers = event.server.persistentData.gunArenaCurrentPlayersCount
 					initializeCurrentPlayers(null, 0)
-					Utils.server.tell("Current Player Count is " + currentPlayers)
+					Utils.server.tell("Current Player Count is " + currentPlayersCount()[0].Team)
 					//Utils.server.tell("Keys are " + Object.keys(currentPlayers[0]))
-					Utils.server.tell("Length is: " + currentPlayers.length)
+					Utils.server.tell("Length is: " + currentPlayersCount().length)
 					return 1
 					})
 				)
 				.then(Commands.literal('reset').executes(ctx => {
 					
 					initializeCurrentPlayers(0, 1)
-					var currentPlayers = event.server.persistentData.gunArenaCurrentPlayersCount
-					Utils.server.tell("Current Player Count is " + currentPlayers)
+					Utils.server.tell("Current Player Count is " + currentPlayersCount())
 					
 					return 1
 					})
 				)
 				.then(Commands.literal('build').executes(ctx => {
 					//USELESS, FOR REMOVAL
-					pushCurrentPlayers()
-					var currentPlayers = event.server.persistentData.gunArenaCurrentPlayersCount
-					Utils.server.tell("Pushed to Current Players, and it is now: " + currentPlayers)
+					//Object.assign(event.server.persistentData.gunArenaCurrentPlayersCount[0], {Kils: "0"})
+					Utils.server.tell("Player 1 is " + event.server.persistentData.gunArenaCurrentPlayersCount[0])
+					//event.server.persistentData.gunArenaCurrentPlayersCount[0]['Kills'] = "0"
+					Utils.server.tell("Pushed to Current Players, and it is now: " + currentPlayersCount())
 					
 					return 1
 					})
 				)
 			)
-			/*.then(Commands.literal('pre-stage-three').executes(ctx => {
+			.then(Commands.literal('pre-stage-three').executes(ctx => {
 				
-				var timer = 30
-				var currentPlayersCount = event.server.persistentData.gunArenaCurrentPlayersCount.length
-				
+				if (currentPlayersCount().length < minPlayers || isPrematchTimerTicking) {return;}
+				prematchTimer = prematchTimerDefault
+				Utils.server.tell("Minimum amount of players are present. Match starting soon!")
 				//Periodic checks to see if a match is viable; that is, two or more players are registered
-				do { 
-				event.scheduleInTicks(20, callback => {
+				recursiveTimer()
+				isPrematchTimerTicking = true
 				
-				if ()
+				function recursiveTimer(){
 					
-					
-				})
+					if (prematchTimer <= 0) {Utils.server.tell("boom baby"); return; }
+					Utils.server.tell("(loop) Match starts in " + prematchTimer)
+					prematchTimer--;
+					event.server.scheduleInTicks(20, callback => {
+						recursiveTimer();
+					})
 					
 				}
-				while ()
+				
 
-				//Add code here
-				//For copy-pasting purposes
-				//KEEP RETURN
+				//return
 				return 1
 				})
-			)*/
+			)
 			.then(Commands.literal('test').executes(ctx => {
 				let tags = ctx.source.entity.stages.has('test_tag')
 				ctx.source.entity.addTag('test_tag')
@@ -259,6 +292,14 @@ onEvent("command.registry", event => {
 				//KEEP RETURN
 				return 1
 				})
+			)
+			.then(Commands.literal('join-team')
+				.then(Commands.literal('blue').executes(ctx => {
+					
+					assignToTeam(0, "Blue")
+					return 1
+					})
+				)
 			)
 			.then(Commands.literal('BLANK').executes(ctx => {
 
