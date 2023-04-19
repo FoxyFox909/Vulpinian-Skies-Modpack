b9851e96ce7e6eed132937924227a3470d51c59e
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-04-19 00:03:30 -0400

    Delete Vulpinian Skies-1.0b3/overrides directory

--- Vulpinian Skies-1.0b3/overrides/kubejs/client_scripts/vulpinian_skies_client.js
+++
@@ -1,9 +0,0 @@
-// priority: 0
-
-console.info('Hello, World! (You will see this line every time client resources reload)')
-
-onEvent('jei.hide.items', event => {
-	// Hide items in JEI here
-	// event.hide('minecraft:cobblestone')
-	event.hide('tconstruct:gadgets/throwball/efln_ball')
-})
