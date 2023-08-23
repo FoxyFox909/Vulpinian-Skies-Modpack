2cb828eba1d605f0ed762b0a3e9aa0736f9ebbb4
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-08-23 16:03:20 -0400

    Fix solcarrot script bug
    new health was not printing properly. Function added to get base max hp + solcarrot additional health, and formatted as per the Discord poll results.

--- MAIN_MODPACK_INSTANCE/kubejs/server_scripts/vulpinian_solcarrot_server.js
+++ MAIN_MODPACK_INSTANCE/kubejs/server_scripts/vulpinian_solcarrot_server.js
@@ -9,8 +9,8 @@ onEvent('item.food_eaten', event => {
         if (uniqueFoods > preUniqueFoods) {
             let nextFood = foodInterval - (uniqueFoods % foodInterval);
             if (nextFood == foodInterval) {
-                let newHealth = event.player.fullNBT['Health'];
-                event.player.tell(Text.of(`The flavors of this world make you stronger! Your new max health is:`).darkAqua().append(Text.of(` ${newHealth}`).darkRed()));
+                let newHealth = getPlayerMaxHP(event);
+                event.player.tell(Text.of(`The flavors of this world make you stronger! Your new Max Health is:`).darkAqua().append(Text.of(` ${newHealth} (${newHealth / 2} Hearts)`).darkRed()));                
             } else {
                 let isSingular = nextFood == 1;
                 event.player.tell(Text.of(`New food tasted! You need ${nextFood} more unique food${isSingular ? "" : "s"} to increase your max health.`).darkAqua());
@@ -18,3 +18,25 @@ onEvent('item.food_eaten', event => {
         }        
     });
 })
+
+function getPlayerMaxHP(event) {
+
+    let maxHealth = 0;
+	for (let i = 0, len = event.player.fullNBT['Attributes'].length; i < len; i++) {
+        if (event.player.fullNBT['Attributes'][i]['Name'].includes('minecraft:generic.max_health')) {
+
+            let solcarrotHealth = 0;
+
+            for (let j = 0, len2 = event.player.fullNBT['Attributes'][i]['Modifiers'].length; j < len2; j++) {
+                
+                if (event.player.fullNBT['Attributes'][i]['Modifiers'][j]['Name'].includes('Health Gained from Trying New Foods')) {
+                    solcarrotHealth = event.player.fullNBT['Attributes'][i]['Modifiers'][j]['Amount'];
+                    break;
+                }
+            }
+            maxHealth = event.player.fullNBT['Attributes'][i]['Base'] + event.player.fullNBT['Attributes'][i]['Modifiers'][0]['Amount'];            
+            break;
+        }
+    }
+    return maxHealth;
+}
